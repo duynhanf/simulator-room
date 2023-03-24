ab22728b6ceed7797dda737caeff7049dc9226ec
duynhanf <duynhanf@gmail.com>
2023-03-24 16:27:01 +0900

    create app wireframe

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
-import { Tooltip } from "@mui/material";
-import "./App.css";
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+  const [position, setPosition] = useState({ x: 0, y: 0 });
 
-      <Tooltip title="This is Tooltip">
-        <button>Test Tooltip</button>
-      </Tooltip>
-    </div>
+  return (
+    <>
+      <div className="flex flex-col md:flex-row mt-16">
+        <div className="sidebar border-2 w-full md:w-1/4 ">
+          <h1>Room 1</h1>
+          <p>Top : {position.x}px</p>
+          <p>Left : {position.y}px</p>
+        </div>
+        <div className="main border-2 w-full md:w-3/4 h-full border-l-0">
+          <h1>Main frame</h1>
+        </div>
+      </div>
+    </>
   );
 }
 
