3cff2ecaa6c6afb662e560f15890039c67966a0b
duynhanf <duynhanf@gmail.com>
2023-03-24 16:15:29 +0900

    feat: add mui and mui icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import { Tooltip } from "@mui/material";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
+
+      <Tooltip title="This is Tooltip">
+        <button>Test Tooltip</button>
+      </Tooltip>
     </div>
   );
 }
