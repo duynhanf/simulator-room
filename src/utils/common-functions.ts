eb9007649bd0040588de308112f1c02079ecd324
duynhanf <duynhanf@gmail.com>
2023-03-27 17:16:42 +0900

    docs: add docs for functions

--- src/utils/common-functions.ts
+++ src/utils/common-functions.ts
@@ -1,12 +1,19 @@
 import { Vector3 } from '@babylonjs/core';
 
+/**
+ * Check mouse position whether inside draggable area or not
+ * @param targetPosition mouse's position
+ * @param groundSize size of ground
+ * @param boxSize size of box
+ * @returns true - if box is inside area, false - if box is outside area
+ */
 export const validateDragArea = (
   targetPosition: Vector3,
   groundSize: number,
   boxSize: number
 ) => {
   return (
-    Math.abs(targetPosition.x) <= groundSize / 2 - boxSize / 2 &&
-    Math.abs(targetPosition.z) <= groundSize / 2 - boxSize / 2
+    Math.max(Math.abs(targetPosition.x), Math.abs(targetPosition.z)) <=
+    groundSize / 2 - boxSize / 2
   );
 };
