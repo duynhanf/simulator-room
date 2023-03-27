3d0f6f0e539e6b6123dc1b68686d24578b1873a3
duynhanf <duynhanf@gmail.com>
2023-03-27 12:58:45 +0900

    refactor: add MainLayout component

---
+++ src/components/templates/MainLayout.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Footer from "../organisms/Footer";
+import Header from "../organisms/Header";
+
+interface Props {
+  children: React.ReactNode | JSX.Element;
+}
+
+const MainLayout: React.FC<Props> = ({ children }) => {
+  return (
+    <>
+      <Header />
+      <div>{children}</div>
+      <Footer />
+    </>
+  );
+};
+
+export default MainLayout;
