302dc38722811c6bf828a97234f2ec7bfc3dcef8
duynhanf <duynhanf@gmail.com>
2023-03-27 17:16:23 +0900

    fix: update footer

--- src/components/organisms/Footer.tsx
+++ src/components/organisms/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <div className="mt-16 text-center">
-      Copyright ©2023 duynhanf. All rights reserved.
+      Copyright ©2023 Room . All rights reserved.
     </div>
   );
 };
