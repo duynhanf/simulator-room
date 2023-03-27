15af0620b7b0bc2ad468cad2e6841fbfc712f6da
duynhanf <duynhanf@gmail.com>
2023-03-27 12:55:01 +0900

    add Footer

---
+++ src/components/organisms/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="mt-16 text-center">
+      Copyright ©2023 duynhanf. All rights reserved.
+    </div>
+  );
+};
+
+export default Footer;
